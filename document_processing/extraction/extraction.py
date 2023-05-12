b3060dcdb3f48838f1180547e98895ba27147459
Marc Mezger <marc.mezger@gmail.com>
2023-05-12 08:02:06 +0200

    Refactoring.

--- document_processing/extraction/extraction.py
+++ document_processing/extraction/extraction.py
@@ -1,14 +1,25 @@
+"""Extraction of the information in the text with luminous."""
 import os
 import re
+
+from aleph_alpha_client import Client, CompletionRequest, Prompt
 from jinja2 import Template
 from loguru import logger
-from aleph_alpha_client import Client,Prompt,CompletionRequest
 
 regex = r"\|\w+\|(.*)\|"
 
 
-def generate_prompt(prompt_name:str, text:str):
-    with open(os.path.join("prompts", prompt_name), "r") as f:
+def generate_prompt(prompt_name: str, text: str) -> str:
+    """Generate the prompt for the luminous api out of a jinja template.
+
+    :param prompt_name: Name of the File with the jinja template
+    :type prompt_name: str
+    :param text: The text to be inserted into the template
+    :type text: str
+    :return: The generated prompt
+    :rtype: str
+    """
+    with open(os.path.join("prompts", prompt_name)) as f:
         prompt = Template(f.read())
 
     # replace the value text with jinja
@@ -19,22 +30,33 @@ def generate_prompt(prompt_name:str, text:str):
     return prompt
 
 
-def send_request(text:str, token:str):
+def send_request(text: str, token: str) -> str:
+    """Send the request to the luminous api.
 
+    :param text: The prompt to be sent to the api
+    :type text: str
+    :param token: The token for the luminous api
+    :type token: str
+    :return: The response from the api
+    :rtype: str
+    """
     client = Client(token=token)
-    request = CompletionRequest(
-    prompt=Prompt.from_text(text),
-    maximum_tokens=256, stop_sequences=["###"]
-    )
+    request = CompletionRequest(prompt=Prompt.from_text(text), maximum_tokens=256, stop_sequences=["###"])
     response = client.complete(request, model="luminous-supreme")
 
     return response.completions[0].completion
 
 
-def extract_information_from_table(model_output:str):
+def extract_information_from_table(model_output: str) -> re.Match:
+    """Extract the information from the table using the regex.
 
+    :param model_output: The output from the luminous api
+    :type model_output: str
+    :return: The matches from the regex
+    :rtype: re.Match
+    """
     # extract the information from the table using the regex
-    matches = re.findall(regex, f'|{model_output}')
+    matches = re.findall(regex, f"|{model_output}")
     logger.info(matches)
 
     return matches
--- document_processing/validation/validate.py
+++ document_processing/validation/validate.py
@@ -1,11 +1,18 @@
+"""Validation of the outputs of the OCR/LLM pipeline."""
 import pandas as pd
-from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
-from loguru import logger
 from Levenshtein import distance
+from loguru import logger
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 
-def validate_ouputs(outputs:str, name:str):
 
+def validate_ouputs(outputs: str, name: str):
+    """Validate the outputs of the OCR/LLM pipeline.
 
+    :param outputs: Outputs from the Luminous API.
+    :type outputs: str
+    :param name: Name of the file to be validated.
+    :type name: str
+    """
     # first load the files for the validation
     if name == "privatrechnung-vorlage.txt":
         rechnung = pd.read_csv("solutions/privatrechnung-vorlage.txt", header=None)
@@ -21,17 +28,15 @@ def validate_ouputs(outputs:str, name:str):
 
     # calculate the metrics
     accuracy = accuracy_score(rechnung, outputs)
-    f1 = f1_score(rechnung, outputs, average='macro')
-    precision = precision_score(rechnung, outputs, average='macro')
-    recall = recall_score(rechnung, outputs, average='macro')
+    f1 = f1_score(rechnung, outputs, average="micro")
+    precision = precision_score(rechnung, outputs, average="micro")
+    recall = recall_score(rechnung, outputs, average="micro")
 
     logger.info(f"Accuracy: {accuracy}")
     logger.info(f"F1: {f1}")
     logger.info(f"Precision: {precision}")
     logger.info(f"Recall: {recall}")
 
-
     # loop over the outputs and calculate the levenshtein distance
     for i in range(len(rechnung)):
         logger.info(f"Levenshtein distance: {distance(rechnung[i], outputs[i])}")
-    
